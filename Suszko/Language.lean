4a62809bda00344f52ce65991ed70eb9bc1a288e
James Oswald <jamesoswald111@gmail.com>
2025-04-19 05:06:09 -0400

    We are ascending to languagehood

--- Suszko/Language.lean
+++ Suszko/Language.lean
@@ -5,17 +5,19 @@ import Mathlib.Data.Set.Image
 import Mathlib.Data.Finite.Defs
 import Mathlib.Data.Fintype.Card
 
-
-inductive ParseTree {α : Type} (c : (n : Nat) -> (Set ((Fin n → α) → α))) (a : α) where
-| leaf : (f : (Fin 0 → α) → α) -> c 0 f -> (arg : (Fin 0 → α)) -> f arg = a -> ParseTree c a
-| node :
-  (n : Nat) -> (f : (Fin n → α) → α) -> c n f ->
-  (args : (Fin n → α)) -> ((i : Fin n) → ParseTree c (args i)) ->
-  f arg = a -> ParseTree c a
-
-
-
 section Language
+
+-- Putting the (a : α) after the : is required! See:
+-- https://proofassistants.stackexchange.com/questions/4326/defining-a-parameterized-property-in-lean-4
+inductive ParseTree
+  {α : Type}
+  (c : (n : Nat) -> (Set ((Fin n -> α) → α))) :
+  (a : α) -> Type
+where
+| node {a} (n : Nat) (f : (Fin n -> α) → α) (args : (Fin n -> α)) :
+  c n f -> (∀ (i : Fin n), ParseTree c (args i)) ->
+  f args = a -> ParseTree c a
+
 /-
 Kadlecikova 2025 page 11:
 "A language L ... is the set of formulae built on the
@@ -27,23 +29,25 @@ connectives from C."
 class Language (α : Type) where
   -- Equality of formulae is decidable
   α_deq : DecidableEq α
-  -- Data: The set of atoms in the language
-  atoms : Set α
+  -- Data: The set of atoms and connectives in the language
+  -- Atoms are treated as 0-place connectives
+  cons (n : Nat) : Set ((Fin n -> α) → α)
   -- Condition: The set of atoms is non-empty
-  atoms_ne : atoms.Nonempty
-  -- Condition: atoms are a subset of the sentences
-  atoms_subset : atoms ⊆ @Set.univ α
-  -- Data: sets of n-place connectives, indexed by their arity
-  cons (n : Nat) : Set (Vector α n → α)
-  -- Condition: The language is closed under the connectives
-  -- i.e. we can construct all possible sentences from the
-  -- connectives and the atoms
-  sents_subset : sorry
-    --∀ a : α : ∃
+  atoms_ne : (cons 0).Nonempty
+  -- The language is closed under the connectives
+  -- IE, every element of the language has a valid parse tree
+  -- under the connectives
+  sents_subset : ∀ α, Nonempty (ParseTree cons α)
 
 -- The following are helpers to bundle real connectives
 -- often in the form of α -> ...n -> α into
 -- functions of type (Fin n → α) -> α
+
+def Language.bundleZero {α} (f : α) : (Fin 0 → α) -> α :=
+  λ _ => f
+
+instance {α}: Coe α ((Fin 0 → α) -> α) where
+  coe := Language.bundleZero
 
 def Language.bundleOne {α} (f : α -> α) : (Fin 1 → α) -> α :=
   λ f1 => f (f1 0)
@@ -63,26 +67,41 @@ inductive PropFormula : Type
 | atom : String -> PropFormula
 | not : PropFormula -> PropFormula
 | and : PropFormula -> PropFormula -> PropFormula
-| or : PropFormula -> PropFormula -> PropFormula
-| impl : PropFormula -> PropFormula -> PropFormula
-| iff : PropFormula -> PropFormula -> PropFormula
 deriving Inhabited, DecidableEq, Repr
 
+def PropFormula.connectives (n : Nat) : Set ((Fin n -> PropFormula) -> PropFormula) :=
+  match n with
+    | 0 => {↑(PropFormula.atom s) | s : String}
+    | 1 => {↑PropFormula.not}
+    | 2 => {↑PropFormula.and}
+    | _ => ∅
 
 instance : Language PropFormula where
   α_deq := inferInstance
-  atoms := {PropFormula.atom _}
-  atoms_ne := ⟨PropFormula.atom "a", rfl⟩
-  atoms_subset := by simp only [Set.subset_univ]
-  cons n :=
-    match n with
-      | 0 => ∅
-      | 1 => {↑PropFormula.not}
-      | 2 => {↑PropFormula.and, ↑PropFormula.or, ↑PropFormula.impl, ↑PropFormula.iff}
-      | _ => ∅
+  cons := PropFormula.connectives
+  atoms_ne := by
+    simp only [Set.Nonempty, Set.mem_setOf_eq];
+    exists (λ _ => PropFormula.atom "a")
+    exists "a"
   sents_subset := by
-    intro n f a a_1
-    simp_all only [Set.mem_univ]
-    sorry
+    intro f
+    induction f
+    . case atom s =>
+      apply Nonempty.intro
+      apply ParseTree.node 0 (PropFormula.atom s) (λ _ => PropFormula.atom s)
+      . simp only [PropFormula.connectives, setOf, exists_apply_eq_apply]
+      . apply Fin.elim0
+      . rfl
+    . case not f ih =>
+      apply Nonempty.intro
+      apply ParseTree.node 1 ↑PropFormula.not (λ _ => PropFormula.not f)
+      . simp only [PropFormula.connectives, setOf, exists_apply_eq_apply]
+        rfl
+      . intro i
+        sorry
+      . sorry
+    . case and f1 f2 ih1 ih2 =>
+      sorry
+
 
 end Language
